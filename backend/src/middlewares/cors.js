fd7943a9f16dbd5cef28ed39874ab9eb5bf172b2
retreatbookroom <retreatbookroom@gmail.com>
2025-06-18 10:14:23 +0800

    Update cors.js

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -13,7 +13,8 @@ const allowedOrigins = [
   'http://127.0.0.1:8080',
   'http://127.0.0.1:8081',
   'http://127.0.0.1:5500',
-  'https://chu-frontend-production.up.railway.app'
+  'https://chu-frontend-production.up.railway.app',
+  'https://retreatbookroom.vercel.app'
 ];
 
 const corsOptions = {
